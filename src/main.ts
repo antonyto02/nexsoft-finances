ee41ef7b174b83cdcc7edc50cb3ec381fec03857
Antonio Delgado <tonydelro@gmail.com>
2025-06-28 11:56:36 -0600

    Habilitar CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://nexusutd.online',
+    ],
+    credentials: true,
+  });
+
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
 }
