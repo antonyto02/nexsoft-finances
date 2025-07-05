172d065bc2ad4da4ed42d57b0bf48b008c434e9a
Antonio Delgado <tonydelro@gmail.com>
2025-07-05 09:01:17 -0600

    feat: add transaction update endpoint

---
+++ src/transactions/dto/update-transaction.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsDateString,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class UpdateTransactionDto {
+  @IsDateString()
+  @IsNotEmpty()
+  date: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(['income', 'expense'])
+  type: string;
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  method: string;
+
+  @IsString()
+  @IsNotEmpty()
+  concept: string;
+}
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Param, Post } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
 
-@Controller('transactions')
+@Controller('finances/transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
   async create(@Body() dto: CreateTransactionDto) {
     const data = await this.transactionsService.create(dto);
+    return data;
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateTransactionDto) {
+    const data = await this.transactionsService.update(id, dto);
     return data;
   }
 }
